898ac3ea93ff900e7c17f32d80c097dd91c6c462
TomasFontana <Tfontana@udesa.edu.ar>
2023-09-04 10:26:22 -0300

    arrancando el header

---
+++ trabajo-integrador-react/src/Componentes/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar (){
+    return (
+        <nav>
+            <ul className="main-nav">
+               <li><Link to="/">Inicio</Link></li>
+               <li><Link to="/favoritos">Favoritos</Link></li>
+               <li><Link to="/peliculasPopulares">Peliculas populares</Link></li>
+               <li><Link to="/PeliculasCartel">Cartelera</Link></li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Navbar
