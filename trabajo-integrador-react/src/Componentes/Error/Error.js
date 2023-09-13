90c8bec2028e41fd7bae24079b28c6d2efd3bc16
TomasFontana <Tfontana@udesa.edu.ar>
2023-09-13 12:54:39 -0300

    pagina Error

---
+++ trabajo-integrador-react/src/Componentes/Error/Error.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+function Error(){
+    return(
+        <div>
+            <p className="error">Esta pagina aun no existe.</p>
+        <link to={'/'}>
+           <p className='error'>Haga click aqui para volver al inicio</p>
+        </link>
+        </div>
+    )
+}
+
+export default Error;
